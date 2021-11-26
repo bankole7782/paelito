b6307effe9377dd4772a95c8b279475da74144c4
Bankole Ojo <ojobankole@gmail.com>
2021-11-26 03:49:44 +0100

    moved paelito ubuntu tarball to google cloud storage.
    now using changing links

--- install_paelito.sh
+++ install_paelito.sh
@@ -7,7 +7,7 @@ apt install libwebkit2gtk-4.0-dev -y
 
 echo "Fetching Assets"
 mkdir -p /opt/saenuma/paelito
-wget -q https://www.saenuma.com/static/paelito.tar.xz -O /opt/saenuma/paelito.tar.xz
+wget -q https://storage.googleapis.com/pandolee/paelito/20211126T034720WAT/paelito.tar.xz -O /opt/saenuma/paelito.tar.xz
 tar -xf /opt/saenuma/paelito.tar.xz -C /opt/saenuma/paelito
 cp /opt/saenuma/paelito/paelito.desktop /usr/share/applications
 chmod +x /opt/saenuma/paelito/paelito
