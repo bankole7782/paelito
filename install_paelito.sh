8fb28160aed342dcbbf4d7ac77e3f331b3b2fff4
Bankole Ojo <ojobankole@gmail.com>
2021-11-26 03:41:30 +0100

    updated install script
    changed the location of the tarball containing the ubuntu program

--- install_paelito.sh
+++ install_paelito.sh
@@ -7,7 +7,7 @@ apt install libwebkit2gtk-4.0-dev -y
 
 echo "Fetching Assets"
 mkdir -p /opt/saenuma/paelito
-wget -q https://storage.googleapis.com/pandolee/paelito.tar.xz -O /opt/saenuma/paelito.tar.xz
+wget -q https://www.saenuma.com/static/paelito.tar.xz -O /opt/saenuma/paelito.tar.xz
 tar -xf /opt/saenuma/paelito.tar.xz -C /opt/saenuma/paelito
 cp /opt/saenuma/paelito/paelito.desktop /usr/share/applications
 chmod +x /opt/saenuma/paelito/paelito
